c10d19d6dee538532e998508584d270b89fc0fe1
Mike Louviere <mike.louviere@hansoninc.com>
2020-06-17 14:42:47 -0400

    first commit

---
+++ src/components/BMI.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import BmiCalculator from './BmiCalculator';
+
+
+const BMI = () => {
+	const [bmiValue, setBmiValue] = useState(0);
+
+	const getBmiClass = bmi => {
+		if ( bmi >= 0 && bmi <= 18.5 ) return 'Underweight';
+		if ( bmi >= 18.5 && bmi <= 24.9 ) return 'Normal Weight';
+		if ( bmi >= 24.9 && bmi <= 29.9 ) return 'Overweight';
+		if ( bmi >= 30 ) return 'Obese';
+	}
+
+	const bmiBackgroundColor = bmi => {
+		if ( bmi >= 0 && bmi <= 18.5 ) return '#FED88B';
+		if ( bmi >= 18.5 && bmi <= 24.9 ) return '#80ff80';
+		if ( bmi >= 24.9 && bmi <= 29.9 ) return '#FF7F50';
+		if ( bmi >= 30 ) return '#FF5411';
+	}
+
+	const bmiCategory = getBmiClass(bmiValue);
+
+	let bmiClass = '';
+
+	if (bmiValue >= 0 && bmiCategory) {
+		bmiClass = bmiCategory.split(' ')[0].toLowerCase();
+	}
+
+	return (
+		<div>
+			<div className="calculator" style={{ backgroundColor: bmiBackgroundColor(bmiValue) }}>
+				<h3>Body Mass Index Calculator</h3>
+				<div className="bmi-result-container">
+					<div className="bmi-result">
+						<div className="bmi-result-number">
+							Body Mass Index (BMI) = {bmiValue}
+						</div>
+						<div className={`bmi-category ${bmiClass}`}>
+							{bmiCategory}
+						</div>
+					</div>
+				</div>
+
+				<BmiCalculator getBmiValue={setBmiValue} />
+			</div>
+		</div>
+	)
+}
+
+export default BMI;
+
+//named export would be export { BmiCalculator };
---
+++ src/components/BmiCalculator.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types';
+
+import FormInput from './FormInput';
+
+const BmiCalculator = props => {
+	const { getBmiValue } = props;
+
+	const [heightUnit, setHeightUnit] = useState('cm');
+	const [weightUnit, setWeightUnit] = useState('kg');
+	const [unit , setUnit] = useState('Metric');
+	const [count, setCount] = useState({
+		heightCount: '0',
+		inchesCount: '0',
+		weightCount: '0'
+	});
+
+	const { heightCount, inchesCount, weightCount } = count;
+
+	useEffect(() => {
+		metricBMI(heightCount, weightCount);
+		imperialBMI(heightCount, weightCount, inchesCount);
+
+		// eslint-disable-next-line
+	}, [heightCount, weightCount, inchesCount]);
+
+	const onChangeInput = e => {
+		const { name, value } = e.target;
+
+		setCount(prevState => ({
+			...prevState,
+			[name]: value
+		}));
+	}
+
+
+	const onSelectTag = e => {
+		setUnit(e.target.value);
+		if (e.target.value === 'Metric') {
+			setHeightUnit('cm');
+			setWeightUnit('kg');
+		} else {
+			setHeightUnit('ft');
+			setWeightUnit('lbs');
+		}
+	}
+
+	const metricBMI = (height, weight) => {
+		if ( parseInt(height) > 0 && parseInt(weight) > 0 ) {
+			const heightToMeter = height / 100;
+			const bmi = weight / (heightToMeter * heightToMeter);
+
+			getBmiValue(Math.round(bmi));
+		}
+	}
+
+	const imperialBMI = (height, weight, inches) => {
+		if ( height > 0 && weight > 0 ) {
+			// 12 inches = ft
+			// convert ft to in
+			// add it to the inches value
+
+			const heightToInches = (height * 12) + parseInt(inches);
+
+			const bmi = 703 * (weight / (heightToInches * heightToInches));
+
+			getBmiValue(Math.round(bmi));
+		}
+	}
+
+	const resetData = e => {
+		e.preventDefault();
+		
+		getBmiValue(0);
+		setUnit('Metric');
+
+		setCount({
+			heightCount: '0',
+			inchesCount: '0',
+			weightCount: '0'
+		});
+
+		setHeightUnit('cm');
+		setWeightUnit('kg');
+	}
+
+	return (
+		<div>
+			<div className="bmi-inputs">
+				<div className="input-fields">
+					<div>
+						<span className="label-unit">Unit</span>
+						<div className="unit">
+							<select
+								name="unit"
+								value={unit}
+								onChange={onSelectTag}
+								className="form-control form-control-sm"
+							>
+								<option value="Metric">Metric</option>
+								<option value="Imperial">Imperial</option>
+							</select>
+						</div>
+					</div>
+					<FormInput 
+						type="text"
+						name="heightCount"
+						title={`Height (${heightUnit})`}
+						value={heightCount}
+						onChange={onChangeInput}
+					/>
+					{
+						unit === 'Imperial' ? 
+						<FormInput 
+							type="text"
+							name="inchesCount"
+							title={` (in)`}
+							value={inchesCount}
+							onChange={onChangeInput}
+						/> : ''
+					}
+					
+					<FormInput 
+						type="text"
+						name="weightCount"
+						title={`Weight (${weightUnit})`}
+						value={weightCount}
+						onChange={onChangeInput}
+					/>
+				</div>
+
+				<button className="button" type="submit" onClick={resetData}>Reset</button>
+			</div>
+		</div>
+	)
+}
+
+BmiCalculator.propTypes = {
+	getBmiValue: PropTypes.func.isRequired
+}
+
+
+export default BmiCalculator
